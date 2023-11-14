548908e02254c1ede25384969429e51ad6cc06f1
Nairobi Montiel <143548150+nkmserrano@users.noreply.github.com>
2023-11-14 16:07:40 -0700

    Update editarProceso.php

--- editarProceso.php
+++ editarProceso.php
@@ -1,21 +1,22 @@
 <?php
     print_r($_POST);
-    if(!isset($_POST['codigo'])){
+    if(!isset($_POST['id_opinion'])){
         header('Location: index.php?mensaje=error');
     }
 
     include 'model/conexion.php';
+    $id_opinion = $_POST['id_opinion'];
     $id_cliente = $_POST["num_id_cliente"];
-    $fecha_opinion = $_POST["fecha_opinion"];
+    $fecha_opi = $_POST["fecha"];
     $calificacion = $_POST["cali"];
     $queja = $_POST["txtqueja"];
     $porque = $_POST["txtpq"];
     $sucursal = $_POST["txtsucu"];
     $sugerencia = $_POST["txtsug"];
 
-    $sentencia = $bd->prepare("UPDATE tbl_opiniones SET id_cliente = ?, fecha_opi = ?, calificacion = ? 
-    queja = ?, porque = ?, sucursal = ? sugerencia = ? where id_opinion = ?;");
-    $resultado = $sentencia->execute([$id_cliente, $fecha_opinion, $calificacion, $queja, $porque, $sucursal, $sugerencia, $id_opinion]);
+    $sentencia = $bd->prepare("UPDATE tbl_opiniones SET id_cliente = ?, fecha_opi = ?, calificacion = ? ,
+    queja = ?, porque = ?, sucursal_compra = ?, sugerencia = ? where id_opinion = ?;");
+    $resultado = $sentencia->execute([$id_cliente, $fecha_opi, $calificacion, $queja, $porque, $sucursal, $sugerencia, $id_opinion]);
 
     if ($resultado === TRUE) {
         header('Location: index.php?mensaje=editado');
